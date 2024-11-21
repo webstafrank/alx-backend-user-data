dd90843adda820decf9ee45731ca5ebaa73cc994
webstafrank <franklyndenish1@gmail.com>
2024-11-21 23:25:52 +0000

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,10 @@
 """
 Authentication module
 """
-import bcrypt
+from db import DB
+from user import User
+from typing import Type
+from bcrypt import hashpw, gensalt
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,9 +18,35 @@ def _hash_password(password: str) -> bytes:
     Returns:
         bytes: The salted hash of the password.
     """
-    if not isinstance(password, str):
-        raise TypeError("Password must be a string")
-    # Hash the password
-    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    return hashed
+    return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Type[User]:
+        """
+        Register a new user in the database.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            User: The created user object.
+
+        Raises:
+            ValueError: If a user with the email already exists.
+        """
+        try:
+            # Check if the user already exists
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except Exception:
+            # User not found, continue to register
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
 
