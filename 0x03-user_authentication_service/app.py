0827cf1401741e8f236f29dc03e26d0a842bea2b
webstafrank <franklyndenish1@gmail.com>
2024-11-22 00:01:48 +0000

    Login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 """
-Flask app for user registration
+Flask app for authentication.
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
 AUTH = Auth()
 
-@app.route("/users", methods=["POST"])
-def users():
+@app.route("/sessions", methods=["POST"])
+def login():
     """
-    POST /users route to register a user.
+    POST /sessions route for user login.
 
-    Form data:
-        - email: User email
-        - password: User password
-    
     Returns:
-        JSON response with a message about registration success or failure.
+        Response object:
+            - JSON payload with success message if login succeeds.
+            - 401 status code if login fails.
     """
     email = request.form.get("email")
     password = request.form.get("password")
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 200
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+
+    if not AUTH.valid_login(email, password):
+        abort(401)  # Unauthorized
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
